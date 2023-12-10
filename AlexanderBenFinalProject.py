ad26d8eb20371ece7101acc1838dd0e4bfa39a55
Farrun <alexander.ben128@gmail.com>
2023-12-10 18:42:08 -0500

    Cleaner Code

--- AlexanderBenFinalProject.py
+++ AlexanderBenFinalProject.py
@@ -12,7 +12,7 @@ import json
 import requests 
 
 
-trackingNumber = input('Enter a tracking number ') #input for testing purposes
+trackingNum = input('Enter a tracking number ') #input for testing purposes
 
 service = None #this is used to determin what shipping service was used.
 
@@ -44,11 +44,11 @@ usps = "(" + ")|(".join(usps_patterns) + ")"
 fedex = "(" + ")|(".join(fedex_patterns) + ")"
 ups= "(" + ")|(".join(ups_patterns) + ")"
     
-if re.match(fedex, trackingNumber) != None:
+if re.match(fedex, trackingNum) != None:
         service = 'FedEx'
-elif re.match(ups, trackingNumber) != None:
+elif re.match(ups, trackingNum) != None:
         service = 'UPS'
-elif re.match(usps, trackingNumber) != None:
+elif re.match(usps, trackingNum) != None:
         service = 'USPS'
 
 #print (service) (this is here just for testing purposes. The final code will not have this.)
@@ -56,115 +56,168 @@ elif re.match(usps, trackingNumber) != None:
 #This goes into the FedEx API and retrives the information. First it starts with authentication and
 #then delevers the results
 if service == 'FedEx':
-        #Authorization
-        secretKey = "34beb5df32e246fb8e143a990495a3b6" #Keys for authorization to FedEx API
-        publicKey = "l7807fd372290045a6b50466423b3a7b14"
-        
-        authURL = "https://apis-sandbox.fedex.com/oauth/token" # Setting up authorization
+    secretKey = "34beb5df32e246fb8e143a990495a3b6" #Keys for authorization to FedEx API
+    publicKey = "l7807fd372290045a6b50466423b3a7b14"
+    
+    authURL = "https://apis-sandbox.fedex.com/oauth/token" # Setting up authorization
 
-        authKeys ={
-            "client_id": publicKey,
-            "client_secret": secretKey,
-            "grant_type": "client_credentials"
-        }
-        AuthResponse = requests.request("POST",url=authURL,data=authKeys)
-        AuthToken = json.loads(AuthResponse.text)["access_token"]
-        TrackURL = "https://apis-sandbox.fedex.com/track/v1/trackingNumbers" #searching for tracking information
-        TrackHeader ={
-            "content-type": "application/json",
-            "authorization": "Bearer "+ AuthToken
-        }
-        TrackBody ={
-            "includeDetailedScans": False,
-            "trackingInfo": [
-                {
-                "trackingNumberInfo": {
-                    "trackingNumber": trackingNumber
-                    }
+    authKeys ={
+        "client_id": publicKey,
+        "client_secret": secretKey,
+        "grant_type": "client_credentials"
+    }
+    AuthResponse = requests.request("POST",url=authURL,data=authKeys)
+    AuthToken = json.loads(AuthResponse.text)["access_token"]
+
+    TrackURL = "https://apis-sandbox.fedex.com/track/v1/trackingnumbers"
+    TrackHeader ={
+        "content-type": "application/json",
+        "authorization": "Bearer "+ AuthToken
+    }
+    TrackBody ={
+        "includeDetailedScans": False, 
+        "trackingInfo": [
+            {
+            "trackingNumberInfo": {
+                "trackingNumber": trackingNum
                 }
-            ]
-        }
+            }
+        ]
+    }
 
+    trackingResponse = requests.request("POST",url=TrackURL,data=json.dumps(TrackBody),headers=TrackHeader)
 
-        trackingResponse = requests.request("POST",url=TrackURL,data=json.dumps(TrackBody),headers=TrackHeader)
+    trackInfo = (str(trackingResponse.text))
 
-        status = (str(trackingResponse.text)) #Retrieving Results
+    statusId = {
+                '"statusByLocale": "In transit",' : 'In Transit',
+                '"statusByLocale": "Delivered",' : 'Delivered', 
+                '"statusByLocale": "Picked up",' : 'Picked up',
+                '"statusByLocale": "Ready for pickup",' : 'Ready for Pickup',
+                '"statusByLocale": "Initiated",' : 'Label Created',
+                '"statusByLocale": "Clearance Delay",' : 'Clearance Delay',
+                '"statusByLocale": "Delivery exception",' : 'Delivery Exception',
+                '"statusByLocale": "Shipment exception",' : 'Shipping Exception',
+                '"statusByLocale": "Cancelled",' : 'Cancelled',
+                }
 
-#This storts the results by status of tracking number. (This needs be be cleaned up, but proof of concept is there.)
-        if '"statusByLocale": "In transit",' in status:
-            print(trackingNumber, ' is in transit.')
-
-        elif '"statusByLocale": "Picked up",' in status:
-            print(trackingNumber, 'is in transit.')
-
-        elif '"statusByLocale": "Clearance Delay",' in status:
-            print(trackingNumber, 'is in transit.')
-
-        elif '"statusByLocale": "Initiated",' in status:
-            print(trackingNumber, 'is not in transit.')
-
-        elif '"statusByLocale": "Delivery exception",' in status:
-            print(trackingNumber, 'has a Delivery Exception.')
-
-        elif '"statusByLocale": "Shipment exception",' in status:
-            print(trackingNumber, 'has a Shipping Exception.')
-
-        elif '"statusByLocale": "Delivered",' in status:
-            print(trackingNumber, 'has been delivered.')
-
-        elif '"statusByLocale": "Ready for pickup",' in status:
-            print(trackingNumber, 'is ready for pickup.')
-
-        elif '"statusByLocale": "Cancelled",' in status:
-            print('The shipment for', trackingNumber, 'has been canceled.')
-
-        else:
-            print( trackingNumber, 'is not a valid Tracking Number') # This is to cover is something was sorted but not actual a tracking number.
-else:
-     pass
+    for key in list(statusId):
+        if key in trackInfo:
+            print(statusId[key])
 
 
 #This goes into the UPS API and retrives the information. First it starts with authentication and
 #then delevers the results
 if service == 'UPS':
      #Authorization
-     client_ID = "stMSHwnTtORwszLqtuk75Psec68WFfxtYBHh6yPOyzaO4IRk"
-     client_Secret = "SDGDXnUXXb7E05vnhbNFACaUMg0Apb239FwTAMp3UGZ3zYGXP8Eb9srrG3e9EMj6"
-     authURL = "https://wwwcie.ups.com/security/v1/oauth/token"
+    client_ID = "stMSHwnTtORwszLqtuk75Psec68WFfxtYBHh6yPOyzaO4IRk"
+    client_Secret = "SDGDXnUXXb7E05vnhbNFACaUMg0Apb239FwTAMp3UGZ3zYGXP8Eb9srrG3e9EMj6"
+    authURL = "https://wwwcie.ups.com/security/v1/oauth/token"
 
-     payload = {"grant_type": "client_credentials"}
+    payload = {"grant_type": "client_credentials"}
 
-     headers = {
+    headers = {
     "Content-Type": "application/x-www-form-urlencoded",
     "x-merchant-id": "G0478G"
     }
 
-     AuthResponse = requests.post(authURL, data=payload, headers=headers, auth=(client_ID, client_Secret))
-     AuthToken = json.loads(AuthResponse.text)["access_token"]
-     data = AuthResponse.json()
-      #print(data)
+    AuthResponse = requests.post(authURL, data=payload, headers=headers, auth=(client_ID, client_Secret))
+    AuthToken = json.loads(AuthResponse.text)["access_token"]
+    data = AuthResponse.json()
+    #print(data)
 
-     trackingUrl = "https://wwwcie.ups.com/api/track/v1/details/" + trackingNumber
 
-     query = {
+    trackingNum = '1Z4472039095660416'
+    url = "https://onlinetools.ups.com/api/track/v1/details/"+ trackingNum
+
+    query = {
     "locale": "en_US",
     "returnSignature": "false"
     }
 
-     headers = {
+    headers = {
     "transId": "string",
     "transactionSrc": "testing",
     "Authorization": "Bearer " + AuthToken
     }
 
-     response = requests.get(trackingUrl, headers=headers, params=query)
+    response = requests.get(url, headers=headers, params=query)
 
-     upsStatus = response.json()
+    trackInfo = (str(response.json()))
+    latestStatus = trackInfo[0 :900]
 
-     print(upsStatus) #This Section is not finished and needs the results to be sorted out.
+    statusId = {
+                "'statusCode': '000'" : 'Status Not Available',
+                "'statusCode': '003'" : 'Order Processed: Ready for UPS',
+                "'statusCode': '005'" : 'In Transit',
+                "'statusCode': '006'" : 'On Vehicle for Delivery',
+                "'statusCode': '007'" : 'Shipment Information Voided',
+                "'statusCode': '010'" : 'In Transit: On Time',
+                "'statusCode': '011'" : 'Delivered',
+                "'statusCode': '012'" : 'Clearance in Progress',
+                "'statusCode': '013'" : 'Exception',
+                "'statusCode': '014'" : 'Clearance Completed',
+                "'statusCode': '016'" : 'Held in Warehouse',
+                "'statusCode': '017'" : 'Held for Customer Pickup',
+                "'statusCode': '018'" : 'Delivery Change Requested: Hold',
+                "'statusCode': '019'" : 'Held for Future Delivery',
+                "'statusCode': '020'" : 'Held for Future Delivery Requested',
+                "'statusCode': '021'" : 'On Vehicle for Delivery Today',
+                "'statusCode': '022'" : 'First Attempt Made',
+                "'statusCode': '023'" : 'Second Attempt Made',
+                "'statusCode': '024'" : 'Final Attempt Made',
+                "'statusCode': '025'" : 'Transferred to Local Post Office for Delivery',
+                "'statusCode': '026'" : 'Delivered by Local Post Office',
+                "'statusCode': '027'" : 'Delivery Address Change Requested',
+                "'statusCode': '028'" : 'Delivery Address Changed',
+                "'statusCode': '029'" : 'Exception: Action Required',
+                "'statusCode': '030'" : 'Local Post Office Exception',
+                "'statusCode': '032'" : 'Adverse Weather May Cause Delay',
+                "'statusCode': '033'" : 'Return to Sender Requested',
+                "'statusCode': '034'" : 'Returned to Sender',
+                "'statusCode': '035'" : 'Returning to Sender',
+                "'statusCode': '036'" : 'Returning to Sender: In Transit',
+                "'statusCode': '037'" : 'Returning to Sender: On Vehicle for Delivery',
+                "'statusCode': '038'" : 'Picked Up',
+                "'statusCode': '039'" : 'In Transit by Post Office',
+                "'statusCode': '040'" : 'Delivered to UPS Access Point Awaiting Customer Pickup',
+                "'statusCode': '041'" : 'Service Upgrade Requested',
+                "'statusCode': '042'" : 'Service Upgraded',
+                "'statusCode': '043'" : 'Voided Pickup',
+                "'statusCode': '044'" : 'In Transit to UPS',
+                "'statusCode': '045'" : 'Order Processed: In Transit to UPS',
+                "'statusCode': '046'" : 'Delay',
+                "'statusCode': '047'" : 'Delay',
+                "'statusCode': '048'" : 'Delay',
+                "'statusCode': '049'" : 'Delay: Action Required',
+                "'statusCode': '050'" : 'Address Information Required',
+                "'statusCode': '051'" : 'Delay: Emergency Situation or Severe Weather',
+                "'statusCode': '052'" : 'Delay: Severe Weather',
+                "'statusCode': '053'" : 'Delay: Severe Weather, Recovery in Progress',
+                "'statusCode': '054'" : 'Delivery Change Requested',
+                "'statusCode': '055'" : 'Rescheduled Delivery',
+                "'statusCode': '056'" : 'Service Upgrade Requested',
+                "'statusCode': '057'" : 'In Transit to a UPS Access Point',
+                "'statusCode': '058'" : 'Clearance Information Required',
+                "'statusCode': '059'" : 'Damage Reported',
+                "'statusCode': '060'" : 'Delivery Attempted',
+                "'statusCode': '061'" : 'Delivery Attempted: Adult Signature Required',
+                "'statusCode': '062'" : 'Delivery Attempted: Funds Required',
+                "'statusCode': '063'" : 'Delivery Change Completed',
+                "'statusCode': '064'" : 'Delivery Refused',
+                "'statusCode': '065'" : 'Pickup Attempted',
+                "'statusCode': '066'" : 'Post Office Delivery Attempted',
+                "'statusCode': '067'" : 'Returned to Sender by Post Office',
+                "'statusCode': '068'" : 'Sent to Lost and Found',
+                "'statusCode': '069'" : 'Package Not Claimed',
+                }
 
-else:
-     pass
+    for key in list(statusId):
+        if (key) in latestStatus:
+            print(statusId[key])
+
+    else:
+        pass
 
 if service == 'USPS':
      print('This tracking number is USPS.') #I am not able to obtain USPS API Authorization. The program will guide the user to search the unit manually.
